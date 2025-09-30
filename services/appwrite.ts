f14c4ecc0a29c8e8e559f09c84a4959622c1d072
Arindamsharma12 <117537886+Arindamsharma12@users.noreply.github.com>
2025-09-30 12:08:57 +0530

    trending movies

--- services/appwrite.ts
+++ services/appwrite.ts
@@ -10,6 +10,7 @@ const client = new Client()
   .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!);
 
 const database = new Databases(client);
+
 export const updateSearchCount = async (query: string, movie: Movie) => {
   try {
     const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
@@ -44,4 +45,20 @@ export const updateSearchCount = async (query: string, movie: Movie) => {
   // if a document is found increment the searchCount field
   // if no document is found
   // create a new document in Appwrite database -> 1
+};
+
+export const getTrendingMovies = async (): Promise<
+  TrendingMovie[] | undefined
+> => {
+  try {
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+      Query.limit(5),
+      Query.orderDesc("count"),
+    ]);
+
+    return result.documents as unknown as TrendingMovie[];
+  } catch (error) {
+    console.log(error);
+    return undefined;
+  }
 };
